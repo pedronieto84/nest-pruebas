b421754647dd1440a73a169029aeebec46afb22b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-16 15:05:27 +0100

    QUito todo lo de docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+
     UsersModule,
   ],
   controllers: [AppController],
