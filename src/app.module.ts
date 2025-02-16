12db1351a9dccdb2c989e4ef09f4b1d1a6de4cae
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-16 13:05:17 +0100

    Nestjs Funciona

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { FirestoreModule } from '@nestjs/firestore';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'your_username',
-      password: 'Mnemonic1984.',
-      database: 'your_database',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    FirestoreModule.forRoot({
-      projectId: 'your_project_id',
-      keyFilename: 'path/to/your/serviceAccountKey.json',
-    }),
-    UsersModule,
+
+  
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
