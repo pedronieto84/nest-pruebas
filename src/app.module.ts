780d0b91f1adb2dcf45b62e10123e0e82e5f1b30
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-17 13:08:50 +0100

    Conexion db

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,21 @@ import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonService } from './pokemon/pokemon.service';
 import { PokemonSchema } from './pokemon/pokemon.schema';
 import {ConfigModule} from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    autoLoadEntities: true,
+    synchronize: true,
+    }),
     MongooseModule.forRoot('mongodb://localhost/nest-pokemon'),
     MongooseModule.forFeature([{ name: 'Pokemon', schema: PokemonSchema }]),
     UsersModule,
