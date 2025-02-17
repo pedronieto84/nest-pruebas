07ff09c3fd4bd677b64b4651dc56aeb995a31cd2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-17 13:34:35 +0100

    compani entity

--- src/companies/entities/company.entity.ts
+++ src/companies/entities/company.entity.ts
@@ -1,8 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Department } from '../../departments/entities/department.entity';
-import { Entity, OneToMany } from 'typeorm';
 
 @Entity()
 export class Company {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
     @OneToMany(() => Department, department => department.company)
     departments: Department[];
 }
