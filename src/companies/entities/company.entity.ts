e048c7bf35ee1a1ae5b9224b20bdca08ae69992b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-17 13:33:58 +0100

    Department entities

--- src/companies/entities/company.entity.ts
+++ src/companies/entities/company.entity.ts
@@ -1,1 +1,8 @@
-export class Company {}
+import { Department } from '../../departments/entities/department.entity';
+import { Entity, OneToMany } from 'typeorm';
+
+@Entity()
+export class Company {
+    @OneToMany(() => Department, department => department.company)
+    departments: Department[];
+}
