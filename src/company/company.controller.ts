d796f363c6512d05311f5dd63ba64c2604ad9844
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-19 14:20:38 +0100

    Controller de create COmpany

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,38 @@
-import {Controller} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { Company } from '@prisma/client';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+
+    constructor( private readonly companyService: CompanyService) { }
+
+        @Get()
+        async getAllCompanies():Promise<Company[]> {
+            return this.companyService.getAllCompanies();
+        }
+
+        @Post()
+        async createCompany(@Body() data: any): Promise<Company> {
+            return this.companyService.createCompany(data);
+        }
+
+        @Get(':id')
+        async getCompanyById(@Param('id') id:string): Promise<Company> {
+            return this.companyService.getCompanyById(+id);
+        }
+
+        @Delete(':id')
+        async deleteCompany(@Param('id') id:string): Promise<Company> {
+            return this.companyService.deleteCompany(+id);
+        }
+
+        @Put(':id')
+        async updateCompany(@Param('id') id:string, @Body() data: Company): Promise<Company> {
+            return this.companyService.updateCompany(+id, data);
+        }
+
+
+
+    
+}
