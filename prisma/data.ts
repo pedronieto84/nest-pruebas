ecb94c4846fb69814b4bed23f11155d7318d843d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-25 09:53:16 +0100

    Preparo la mock data

---
+++ prisma/data.ts
@@ -0,0 +1,70 @@
+export const companies: string[] = [
+    "Santander",
+    "BBVA",
+    "Iberdrola",
+    "Inditex",
+    "Repsol",
+    "Telefónica",
+    "Endesa",
+    "CaixaBank",
+    "Aena",
+    "Ferrovial"
+];
+
+export const names: string[] = [
+    // Españoles
+    "Alejandro", "María", "Carlos", "Lucía", "Javier", "Ana", "David", "Sofía", "Daniel", "Laura",
+    "Miguel", "Paula", "Sergio", "Elena", "Hugo",
+    // Anglosajones
+    "William", "Emily", "John", "Olivia", "James",
+    // Alemanes
+    "Hans", "Heidi", "Fritz", "Greta", "Wolfgang",
+    // Rusos
+    "Ivan", "Anastasia", "Dmitri", "Tatiana", "Nikolai",
+    // Chinos
+    "Wei", "Xiao", "Li", "Mei", "Chen",
+    // Italianos
+    "Giovanni", "Francesca", "Luca", "Chiara", "Alessandro"
+];
+
+export const surnames = [
+    // Españoles
+    "García", "Fernández", "González", "Rodríguez", "López", "Martínez", "Sánchez", "Pérez", "Gómez", "Martín",
+    "Jiménez", "Ruiz", "Hernández", "Díaz", "Moreno", "Álvarez", "Muñoz", "Romero", "Alonso", "Gutiérrez",
+    // Anglosajones
+    "Smith", "Johnson", "Brown", "Taylor", "Anderson",
+    // Alemanes
+    "Schmidt", "Müller", "Weber", "Schneider", "Fischer",
+    // Rusos
+    "Ivanov", "Petrov", "Sokolov", "Volkov", "Kuznetsov",
+    // Chinos
+    "Wang", "Zhang", "Li", "Liu", "Huang",
+    // Italianos
+    "Rossi", "Ferrari", "Bianchi", "Romano", "Conti"
+];
+
+export const departments:string[] = [
+    "Marketing Estratégico",
+    "Contabilidad y Finanzas",
+    "Innovación y Desarrollo",
+    "Relaciones Públicas",
+    "Logística y Distribución",
+    "Atención al Cliente",
+    "Desarrollo de Software",
+    "Recursos Humanos",
+    "Investigación de Mercado",
+    "Operaciones y Producción"
+  ];
+
+  export const projects: string[] = [
+    "Expansión Digital",
+    "Optimización Financiera",
+    "Nueva App Móvil",
+    "Sostenibilidad Empresarial",
+    "Automatización de Logística",
+    "Campaña Publicitaria Global",
+    "Plataforma de E-learning",
+    "Análisis de Tendencias de Mercado",
+    "Mejoras en la Atención al Cliente",
+    "Seguridad Cibernética Avanzada"
+  ];
