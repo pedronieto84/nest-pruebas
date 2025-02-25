e73e35ad7726fab31713c74d14c7ea59e930aef2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-25 10:20:03 +0100

    Consigo crear compañías y asociarles departamentos y proyectos.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,67 @@
 import { PrismaClient } from "@prisma/client";
-import { companies, departments, names, surnames, projects } from "./data";
-import { generateUUID } from './../src/helpers/helpers'; 
+import { getCompanies, getDepartments, getNames, getSurnames, getProjects } from "./data";
+import { generateUUID } from './../src/helpers/helpers';
+
 
 const prisma = new PrismaClient();
 
+
+const configObject = {
+    companies: 2,
+    workers: 5,
+    departments: 2,
+    projects: 2
+}
+
 async function main() {
-    const compRecords = await prisma.company.createMany({
-        data: companies.map((company) => ({
+    await prisma.company.createMany({
+        data: getCompanies(configObject.companies).map((company) => ({
             compId: generateUUID(),
             name: company
         })),
-    })
+        skipDuplicates: true
+    });
 
-    
+    // Cojo las ids de las comp created
+    const createdCompaniesId = (await prisma.company.findMany({
+        where: {
+            name: {
+                in: getCompanies(configObject.companies)
+            }
+        },
+        select: {
+            compId: true
+        }
+    })).map(company => company.compId);
 
+    // Tengo que crear proyectos de cada compañías
+    for (const companyId of createdCompaniesId) {
+        await prisma.project.createMany({
+            data: getProjects(configObject.projects).map((project) => ({
+                projId: generateUUID(),
+                name: project,
+                compId: companyId
+            })),
+        });
+    }
+
+     // Tengo que crear departamentos de cada compañía
+     for (const companyId of createdCompaniesId) {
+        await prisma.department.createMany({
+            data: getDepartments(configObject.departments).map((department) => ({
+                deptId: generateUUID(),
+                name: department,
+                compId: companyId
+            })),
+        });
+    }
 }
 
-
 main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
- 
-    
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
+
