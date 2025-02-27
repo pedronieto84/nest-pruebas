0bf16e872f6067a06c9c9d8f77537ab76ab63c62
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-27 15:13:39 +0100

    Creo function para delete all

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,41 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
+import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// Initialize Firebase Admin SDK
+if (!admin.apps.length) {
+    admin.initializeApp();
+}
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const deleteAllUsers = onRequest(async (request, response) => {
+    try {
+        await deleteUsers();
+        response.send("All users have been deleted successfully.");
+    } catch (error) {
+        logger.error("Error deleting users:", error);
+        response.status(500).send("Error deleting users.");
+    }
+});
+
+async function deleteUsers(nextPageToken?: string) {
+    try {
+        const listUsersResult = await admin.auth().listUsers(1000, nextPageToken);
+        const users = listUsersResult.users;
+        const uids = users.map(user => user.uid);
+
+        if (uids.length > 0) {
+            await admin.auth().deleteUsers(uids);
+            logger.info(`Successfully deleted ${uids.length} users`);
+        }
+
+        if (listUsersResult.pageToken) {
+            await deleteUsers(listUsersResult.pageToken);
+        }
+    } catch (error) {
+        logger.error("Error deleting users:", error);
+        throw error;
+    }
+}
