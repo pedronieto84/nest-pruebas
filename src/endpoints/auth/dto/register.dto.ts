cb739a9a4a8b6de7e38eed8277a1321bfaca0f50
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-03-20 11:17:25 +0100

    Quito todo

--- src/endpoints/auth/dto/register.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { LoginAuthDto } from './login.dto';
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
-
-export class RegisterAuthDto extends PartialType(LoginAuthDto) {
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-}
